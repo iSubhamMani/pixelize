afea33f2e4209606200c135b9da537f1a610f3a1
Subham Mani <manisubham09@gmail.com>
2024-03-21 20:49:17 +0530

    Update Navbar component styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import icon from "../../assets/icon.png";
 
 const Navbar = () => {
   return (
-    <div className="px-8 py-2 flex gap-2 items-center cursor-pointer">
+    <div className="px-8 py-2 flex gap-2 items-center">
       <img className="w-8" src={icon} alt="" />
-      <h1 className="text-xl text-accent-clr font-bold">Pixelize</h1>
+      <h1 className="text-xl text-accent-clr font-bold cursor-pointer">
+        Pixelize
+      </h1>
     </div>
   );
 };

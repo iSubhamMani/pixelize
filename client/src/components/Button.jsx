2ec3f3bd271d20e1113e3fb87a1513cdc8a17c6f
Subham Mani <manisubham09@gmail.com>
2024-03-21 20:48:22 +0530

    Add onClick and onChange handlers to Button and InputField components

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,10 @@
-const Button = ({ content }) => {
+const Button = ({ content, onClickHandler }) => {
   return (
     <div className="w-full">
-      <button className="px-5 py-4 w-full bg-secondary-clr text-[1.1rem] text-accent-clr hover:bg-hover-clr transition duration-200 ease-in-out shadow-md font-bold rounded-lg">
+      <button
+        onClick={onClickHandler}
+        className="px-5 py-4 w-full bg-secondary-clr text-[1.1rem] text-accent-clr hover:bg-hover-clr transition duration-200 ease-in-out shadow-md font-bold rounded-lg"
+      >
         {content}
       </button>
     </div>
--- client/src/components/InputField.jsx
+++ client/src/components/InputField.jsx
@@ -1,9 +1,12 @@
-const InputField = ({ type, placeholder }) => {
+const InputField = ({ type, placeholder, name, onChangeHandler }) => {
   return (
     <div>
       <input
         type={type}
+        name={name}
         placeholder={placeholder}
+        onChange={onChangeHandler}
+        autoComplete="off"
         className="px-5 py-4 bg-secondary-clr placeholder:text-text-clr-3 shadow-md w-full min-w-[200px] text-[1.1rem] text-text-clr-1 focus:outline-accent-clr rounded-lg"
       />
     </div>

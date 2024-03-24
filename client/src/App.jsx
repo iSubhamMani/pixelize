b81ab33cacfbc39b705b1077145670e7d3cb8d95
Subham Mani <manisubham09@gmail.com>
2024-03-24 21:57:50 +0530

    Refactor routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,21 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import AuthCheck from "./utils/AuthCheck";
 import Feed from "./pages/Feed";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
 function App() {
-  // Check for auth
-  // Load components based on result
-
   const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <AuthCheck />,
-      children: [
-        {
-          path: "/feed",
-          element: <Feed />,
-        },
-        {
-          path: "/register",
-          element: <Register />,
-        },
-        {
-          path: "/login",
-          element: <Login />,
-        },
-      ],
+      element: <Feed />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
 

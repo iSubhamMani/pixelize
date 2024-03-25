5506890f82d7ae53d1bfd8e0273f00fdbd661487
Subham Mani <manisubham09@gmail.com>
2024-03-25 22:02:09 +0530

    Add Redux store and authentication logic to App and Feed components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Feed from "./pages/Feed";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { Provider } from "react-redux";
+import appStore from "./redux/appStore";
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -21,7 +23,9 @@ function App() {
 
   return (
     <div className="App">
-      <RouterProvider router={appRouter} />
+      <Provider store={appStore}>
+        <RouterProvider router={appRouter} />
+      </Provider>
     </div>
   );
 }
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,5 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import handleTokenRenewal from "../utils/handleTokenRenewal";
+import { useDispatch } from "react-redux";
+import { addUser } from "../redux/slices/user.slice";
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    verifyUser();
+  }, []);
+
+  const verifyUser = async () => {
+    try {
+      const response = await axios.get("/api/v1/users/current-user");
+
+      if (
+        response.data?.status === 200 &&
+        response.data?.message === "User found"
+      ) {
+        setIsAuthenticated(true);
+        // add user to store
+        dispatch(addUser(response.data?.data));
+      }
+    } catch (error) {
+      if (
+        error.response.data?.status === 401 &&
+        error.response.data?.message === "No refresh token"
+      )
+        navigate("/login");
+      else if (
+        error.response.data?.status === 401 &&
+        error.response.data?.message === "Access token expired"
+      ) {
+        handleTokenRenewal(verifyUser, () => {
+          navigate("/login");
+        });
+      }
+    }
+  };
+
+  return isAuthenticated && <div>Feed</div>;
 };
 
 export default Feed;

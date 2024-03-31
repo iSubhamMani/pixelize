45ef8e94257e593c97e4779f10f3ef2a85408182
Subham Mani <manisubham09@gmail.com>
2024-03-31 14:10:06 +0530

    Add multer and cloudinary middleware

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadPhotoToCloudinary = async (imageLocalPath) => {
+  try {
+    if (!imageLocalPath) return;
+
+    const response = await cloudinary.uploader.upload(imageLocalPath, {
+      folder: "pixelize/posts",
+      resource_type: "image",
+    });
+
+    fs.unlinkSync(imageLocalPath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(imageLocalPath);
+    return;
+  }
+};
+
+export { uploadPhotoToCloudinary };

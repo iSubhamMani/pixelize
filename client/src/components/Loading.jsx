5dbda0e072c2bb67cacfb823a1152bb6f63fb68c
Subham Mani <manisubham09@gmail.com>
2024-04-02 20:54:25 +0530

    Add Loading component

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import Spinner from "./Spinner";
+
+const Loading = () => {
+  return (
+    <div className="main-content sm:top-[40%]">
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loading;

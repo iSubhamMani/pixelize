1b73b7efc8bd63a68be302a69905c2b036b5e250
Subham Mani <manisubham09@gmail.com>
2024-04-03 21:29:14 +0530

    Add conditional rendering for Logo and Navbar components based on user

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,16 +2,21 @@ import { Outlet } from "react-router-dom";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import NavbarMobile from "./NavbarMobile";
+import { useSelector } from "react-redux";
 
 const Main = () => {
+  const { user } = useSelector((store) => store.user);
+
   return (
     <div className="min-h-[100vh] bg-primary-clr relative flex flex-col sm:flex-row">
-      <div className="py-4 sm:hidden">
-        <Logo />
-      </div>
-      <Navbar />
+      {user && (
+        <div className="py-4 sm:hidden">
+          <Logo />
+        </div>
+      )}
+      {user && <Navbar />}
       <Outlet />
-      <NavbarMobile />
+      {user && <NavbarMobile />}
     </div>
   );
 };

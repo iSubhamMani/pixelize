f6d9ae73eb03a19095cde1c99341e682d30a7389
Subham Mani <manisubham09@gmail.com>
2024-04-15 20:28:51 +0530

    Refactor like controller and add new routes

--- server/src/controllers/like.controller.js
+++ server/src/controllers/like.controller.js
@@ -1,8 +1,9 @@
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { Like } from "../models/like.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-const togglePostLike = async (req, res) => {
+const togglePostLike = asyncHandler(async (req, res) => {
   const { postId } = req.body;
 
   if (!postId) throw new ApiError(400, "Post ID is required");
@@ -31,6 +32,34 @@ const togglePostLike = async (req, res) => {
       .status(200)
       .json(new ApiResponse(200, "Post unliked successfully", unlike));
   }
-};
+});
 
-export { togglePostLike };
+const getCurrentUserLikeStatus = asyncHandler(async (req, res) => {
+  const { postId } = req.query;
+
+  if (!postId) throw new ApiError(400, "Post ID is required");
+
+  const isLiked = await Like.findOne({ post: postId, owner: req.user?._id });
+
+  if (isLiked) {
+    return res.status(200).json(new ApiResponse(200, "Post is liked", isLiked));
+  } else {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Post is not liked", null));
+  }
+});
+
+const getPostLikesCount = asyncHandler(async (req, res) => {
+  const { postId } = req.query;
+
+  if (!postId) throw new ApiError(400, "Post ID is required");
+
+  const likesCount = await Like.find({ post: postId }).countDocuments();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Post likes count", likesCount));
+});
+
+export { togglePostLike, getCurrentUserLikeStatus, getPostLikesCount };
--- server/src/routes/like.route.js
+++ server/src/routes/like.route.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/auth.middleware.js";
-import { togglePostLike } from "../controllers/like.controller.js";
+import {
+  getCurrentUserLikeStatus,
+  getPostLikesCount,
+  togglePostLike,
+} from "../controllers/like.controller.js";
 
 const likeRouter = Router();
 
 likeRouter.route("/toggle-like").post(verifyToken, togglePostLike);
+likeRouter
+  .route("/get-current-user-like-status")
+  .get(verifyToken, getCurrentUserLikeStatus);
+
+likeRouter.route("/get-likes-count").get(verifyToken, getPostLikesCount);
 
 export default likeRouter;

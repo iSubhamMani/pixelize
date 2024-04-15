2d450b963d0520f1dab80081a0af23a3ff8244cd
Subham Mani <manisubham09@gmail.com>
2024-04-15 20:29:16 +0530

    Add like functionality to Post component

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,12 +1,96 @@
 import convertDateTime from "../utils/dateTimeConverter";
 import { FaRegHeart } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa6";
 import { AiOutlineComment } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import LazyImage from "./LazyImage";
+import axios from "axios";
 import { defaultProfilePhoto } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addCachedPosts,
+  addLikedPosts,
+  deleteLikedPosts,
+  setPostLikesCount,
+} from "../redux/slices/post.slice";
 
 const Post = ({ post }) => {
   const { owner, caption, image, createdAt } = post;
+
+  const { likedPosts, cachedPosts, likeCount } = useSelector(
+    (state) => state.post
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (cachedPosts[post?._id]) return;
+    dispatch(addCachedPosts({ [post._id]: true }));
+    // fetch post likes count
+    getPostLikesCount();
+  }, []);
+
+  useEffect(() => {
+    if (likedPosts[post?._id]) return;
+    getCurrentUserLikeStatus();
+  }, []);
+
+  const togglePostLike = async () => {
+    try {
+      const response = await axios.post(
+        "/api/v1/likes/toggle-like",
+        {
+          postId: post._id,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      getPostLikesCount();
+
+      if (response.data?.status === 201) {
+        // set like status
+        dispatch(addLikedPosts({ [post._id]: true }));
+      } else if (response.data?.status === 200) {
+        // set like status
+        dispatch(deleteLikedPosts(post._id));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getCurrentUserLikeStatus = async () => {
+    try {
+      const response = await axios.get(
+        `/api/v1/likes/get-current-user-like-status?postId=${post._id}`
+      );
+
+      if (response) {
+        if (
+          response.data?.message === "Post is liked" &&
+          response.data?.status === 200
+        )
+          dispatch(addLikedPosts({ [post._id]: true }));
+        else dispatch(deleteLikedPosts(post._id));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getPostLikesCount = async () => {
+    const response = await axios.get(
+      `/api/v1/likes/get-likes-count?postId=${post._id}`
+    );
+
+    if (response) {
+      dispatch(setPostLikesCount({ [post._id]: response.data?.data }));
+    }
+  };
 
   return (
     <div className="my-2 flex flex-col shadow-lg">
@@ -28,7 +112,19 @@ const Post = ({ post }) => {
       <div className="flex-1 rounded-sm overflow-hidden relative mt-3">
         <LazyImage image={image} />
         <div className="w-full absolute bottom-0 px-4 py-3 left-0 flex items-center gap-2 bg-gradient-to-r from-[#000] to-transparent">
-          <FaRegHeart className="text-text-clr-1 icon" />
+          {likedPosts[post?._id] ? (
+            <FaHeart className="text-like-clr icon" onClick={togglePostLike} />
+          ) : (
+            <FaRegHeart
+              className="text-text-clr-1 icon"
+              onClick={togglePostLike}
+            />
+          )}
+          {likeCount[post?._id] !== 0 ? (
+            <span className="text-text-clr-1 text-[0.9rem] sm:text-[1rem]">
+              {likeCount[post?._id]}
+            </span>
+          ) : null}
           <AiOutlineComment className="text-text-clr-1 icon" />
         </div>
       </div>

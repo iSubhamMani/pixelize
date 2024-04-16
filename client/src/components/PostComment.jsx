c10b9e64a88b322e1eb003905d4c7e0659ad825f
Subham Mani <manisubham09@gmail.com>
2024-04-17 01:43:03 +0530

    Add PostComment and Comment components

---
+++ client/src/components/PostComment.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { defaultProfilePhoto } from "../utils/constants";
+import convertDateTime from "../utils/dateTimeConverter";
+import LazyImage from "./LazyImage";
+import { AiFillDelete } from "react-icons/ai";
+import { useSelector } from "react-redux";
+
+const PostComment = ({ comment }) => {
+  const { user } = useSelector((state) => state.user);
+
+  return (
+    <div className="flex items-start gap-3 my-6">
+      <div>
+        <div className="rounded-full overflow-hidden w-[2rem] h-[2rem] sm:w-[2.5rem] sm:h-[2.5rem] border-2 sm:border-[3px] border-accent-clr">
+          <LazyImage
+            image={comment?.owner?.profilePhoto || defaultProfilePhoto}
+          />
+        </div>
+      </div>
+      <div className="flex-1">
+        <div>
+          <p className="text-text-clr-1 font-light text-[0.97rem] sm:text-[1rem]">
+            <Link to={`/u/${comment?.owner?.username}`}>
+              <span className="text-text-clr-1 font-bold mr-2">
+                @{comment?.owner?.username}
+              </span>
+            </Link>
+            {comment?.content}
+          </p>
+        </div>
+        <div className="mt-1">
+          <p className="text-text-clr-1 text-[0.8rem] sm:text-[0.9rem]">
+            {convertDateTime(comment?.createdAt)}
+          </p>
+        </div>
+      </div>
+      {comment?.owner?._id === user?._id && (
+        <div>
+          <button>
+            <AiFillDelete className="text-error-clr text-[1rem] sm:text-[1.25rem]" />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PostComment;
---
+++ client/src/pages/Comment.jsx
@@ -0,0 +1,129 @@
+import { Link, useLocation } from "react-router-dom";
+import convertDateTime from "../utils/dateTimeConverter";
+import LazyImage from "../components/LazyImage";
+import { defaultProfilePhoto } from "../utils/constants";
+import PostComment from "../components/PostComment";
+import { RiSendPlaneFill } from "react-icons/ri";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import Loading from "../components/Loading";
+
+const Comment = () => {
+  const { state } = useLocation();
+  const commentBox = useRef();
+  const { owner, caption, createdAt, postId } = state.postDetails;
+  const [commentContent, setCommentContent] = useState("");
+  const [addCommentLoader, setAddCommentLoader] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  const addComment = async () => {
+    if (commentContent.trim() === "" || !commentContent) return;
+
+    try {
+      setAddCommentLoader(true);
+      const response = await axios.post(
+        `/api/v1/comments/create-comment/${postId}`,
+        {
+          content: commentContent,
+        }
+      );
+
+      if (response) {
+        commentBox.current.value = "";
+        setAddCommentLoader(false);
+        getComments();
+      }
+    } catch (error) {
+      setAddCommentLoader(false);
+      console.log(error);
+    }
+  };
+
+  const getComments = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `/api/v1/comments/get-post-comments/${postId}`
+      );
+
+      if (response) {
+        setLoading(false);
+        setComments(response?.data?.data);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex-1">
+      <div className="min-h-screen pt-6 bg-text-clr-5 w-full sm:w-[70%] min-w-[15.5rem] max-w-[42rem] mx-auto flex flex-col">
+        <div className="px-4 flex items-start gap-3 pb-4 border-b-[1px] border-text-clr-4">
+          <div>
+            <div className="rounded-full overflow-hidden w-[2rem] h-[2rem] sm:w-[2.5rem] sm:h-[2.5rem] border-2 sm:border-[3px] border-accent-clr">
+              <LazyImage image={owner?.profilePhoto || defaultProfilePhoto} />
+            </div>
+          </div>
+          <div>
+            <div>
+              <p className="text-text-clr-1 font-light text-[1rem]">
+                <Link to={`/u/${owner?.username}`}>
+                  <span className="text-text-clr-1 font-bold mr-2">
+                    @{owner?.username}
+                  </span>
+                </Link>
+                {caption}
+              </p>
+            </div>
+            <div className="mt-1">
+              <p className="text-text-clr-1 text-[0.8rem] sm:text-[0.9rem]">
+                {convertDateTime(createdAt)}
+              </p>
+            </div>
+          </div>
+        </div>
+        <p className="my-4 px-4 text-text-clr-1 text-[1rem] sm:text-lg">
+          {comments.length} Comments
+        </p>
+        {loading && (
+          <div className="my-4 flex justify-center items-center">
+            <Loading />
+          </div>
+        )}
+        <div className="px-4 flex-1">
+          {comments.length === 0 ? (
+            <p className="text-center text-text-clr-4 my-4">No comments yet</p>
+          ) : (
+            comments.map((comment) => {
+              return <PostComment comment={comment} key={comment?._id} />;
+            })
+          )}
+        </div>
+        <div className="bg-primary-clr sticky bottom-[49.59px] sm:bottom-0 px-4 py-4 border-b-[2px] border-b-text-clr-4 focus-within:border-b-accent-clr flex items-center gap-2 transition duration-200 ease-in-out z-10">
+          <input
+            className="text-text-clr-1 w-full outline-none bg-transparent"
+            type="text"
+            ref={commentBox}
+            onChange={(e) => setCommentContent(e.target.value)}
+            placeholder="Write a comment"
+          />
+          {!addCommentLoader ? (
+            <button onClick={addComment}>
+              <RiSendPlaneFill className="text-accent-clr" size={24} />
+            </button>
+          ) : (
+            <RiSendPlaneFill className="text-text-clr-4" size={24} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;

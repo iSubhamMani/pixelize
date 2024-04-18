062ab11535c6291bc5d569fd675bb483104ddd3f
Subham Mani <manisubham09@gmail.com>
2024-04-18 19:09:27 +0530

    Add CORS configuration to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
+
+app.use(
+  cors({
+    origin: "https://pixelize-unet.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));

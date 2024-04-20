15e3d1551222b81d332b99fc8f26f019abf73e72
Subham Mani <manisubham09@gmail.com>
2024-04-20 11:40:37 +0530

    Add console.log statement to multer.middleware.js

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -3,6 +3,7 @@ import multer from "multer";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
+    console.log("file", file);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);

577526ead1db61c4edc466bb6c5efe0ab744ce96
Subham Mani <manisubham09@gmail.com>
2024-04-20 18:40:25 +0530

    Add console log statement to display upload directory

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -7,6 +7,7 @@ const storage = multer.diskStorage({
     const dirName = path.dirname(fileURLToPath(import.meta.url));
 
     const uploadDir = path.join(dirName, "../../public/temp");
+    console.log("uploadDir", uploadDir);
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
